d0b2a9a590d131b99a456b9816d5deb73fd7c17d
Jacob Ford <jaycub.k.ford@gmail.com>
2022-02-04 12:04:12 +0000

    feat: created database seeding classes

---
+++ uMini.Infrastructure/Data/ShortUrlDbContextSeed.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace uMini.Infrastructure.Data;
+
+public class ShortUrlDbContextSeed
+{
+    public static async Task SeedAsync(ShortUrlDbContext db, ILogger logger)
+    {
+        try
+        {
+            if (db.Database.IsSqlServer())
+            {
+                db.Database.Migrate();
+            }
+
+            if (!await db.ShortUrls.AnyAsync())
+            {
+                await db.ShortUrls.AddAsync(new ShortUrl
+                {
+                    Key = "google",
+                    Url = "https://google.com/",
+                    CreatorId = "472963c0-6192-4f48-9006-11761bf99657"
+                });
+
+                await db.SaveChangesAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex.Message);
+        }
+    }
+}
---
+++ uMini.Infrastructure/Identity/ApplicationIdentityDbContextSeed.cs
@@ -0,0 +1,27 @@
+﻿namespace uMini.Infrastructure.Identity;
+
+public class ApplicationIdentityDbContextSeed
+{
+    public static async Task SeedAsync(ApplicationIdentityDbContext context, UserManager<IdentityUser> userManager)
+    {
+        var defaultUser = new IdentityUser
+        {
+            Id = "472963c0-6192-4f48-9006-11761bf99657",
+            UserName = "demouser@microsoft.com",
+            Email = "demouser@microsoft.com",
+            PhoneNumber = "+111111111111",
+            EmailConfirmed = true,
+            PhoneNumberConfirmed = true,
+        };
+
+        if (!context.Users.Any(u => u.Email == defaultUser.Email))
+        {
+            var password = new PasswordHasher<IdentityUser>();
+            var hashed = password.HashPassword(defaultUser, "Passw0rd!");
+            defaultUser.PasswordHash = hashed;
+
+            await userManager.CreateAsync(defaultUser);
+        }
+    }
+
+}
