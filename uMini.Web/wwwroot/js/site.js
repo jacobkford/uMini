036281169b01285720512b837d1032aa84878657
Jacob Ford <jaycub.k.ford@gmail.com>
2022-02-05 18:11:52 +0000

    feat: added toast notifications

--- uMini.Web/wwwroot/js/site.js
+++ uMini.Web/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+    // Creates an array of toasts (it only initializes them)
+    return new bootstrap.Toast(toastEl) // No need for options; use the default options
+});
+toastList.forEach(toast => toast.show());
