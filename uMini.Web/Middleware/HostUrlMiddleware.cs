cc7384c2000c22bf2b1a2ba1a906bede4a80d512
Jacob Ford <jaycub.k.ford@gmail.com>
2022-05-16 13:38:00 +0100

    added host url middleware

---
+++ uMini.Web/Middleware/HostUrlMiddleware.cs
@@ -0,0 +1,29 @@
+﻿namespace uMini.Web.Middleware;
+
+public class HostUrlMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public HostUrlMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext httpContext)
+    {
+        var urlScheme = httpContext.Request.Scheme;
+        var urlHost = httpContext.Request.Host;
+        var urlPathBase = httpContext.Request.PathBase;
+
+        httpContext.Items.Add("HostUrl", $"{urlScheme}://{urlHost}{urlPathBase}/");
+        await _next(httpContext);
+    }
+}
+
+public static class HostUrlMiddlewareExtensions
+{
+    public static IApplicationBuilder UseHostUrlMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<HostUrlMiddleware>();
+    }
+}
